3c6687566bf19756e94db334b9c5a49d01165f6c
superdoge6 <mr.bread12345@gmail.com>
2018-02-19 18:23:09 +0200

    more auto
    now auto works in both red sides and works in blue left but only on the right and center colunms

--- gyroDrive.java
+++ gyroDrive.java
@@ -20,15 +20,26 @@ public class gyroDrive extends AutoMain {
         connectionInit();
         robot.setMotorDriveMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robot.setMotorDriveMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        final int MIDDLE_TICKS = -3710;// works for red left
-        final int LEFT_TICKS = -4420;// works for red left
-        final int RIGHT_TICKS = -2840;//works but only tested once for red left
-//
+        final int MIDDLE_TICKS_LEFT_R = -3710;// works for red left
+        final int LEFT_TICKS_LEFT_R = -4420;// works for red left
+        final int RIGHT_TICKS_LEFT_R = -2800;//works red left
+        final int MIDDLE_TICKS_RIGHT_R = -1760;// works for red left
+        final int LEFT_TICKS_RIGHT_R = -2485;// works for red left
+        final int RIGHT_TICKS_RIGHT_R = -800;//works red left
+
+        final int MIDDLE_TICKS_RIGHT_B = -3710;//
+        final int LEFT_TICKS_RIGHT_B = -4420;//
+        final int RIGHT_TICKS_RIGHT_B = -2800;//
+        final int MIDDLE_TICKS_LEFT_B = -1760;// works in blue left
+        final int LEFT_TICKS_LEFT_B = -2485;//  works in blue left
+        final int RIGHT_TICKS_LEFT_B = -800;//
+
+
 
         waitForStart();
         //Red Left - works!
-
-        gyroDrive(-0.3, RIGHT_TICKS, 0);
+        /*
+        gyroDrive(-0.3, LEFT_TICKS, 0);
         gyroTurn(0.3, -90);
         gyroHold(0.3, -90, 0.5);
         gyroDrive(-0.3, -600, -90);
@@ -36,5 +47,37 @@ public class gyroDrive extends AutoMain {
         gyroDrive(-0.3, -1100, -90);
         robot.cubePickUpSpeed(0.0);
         gyroDrive(-0.3, 600, -90);
+        */
+
+        //red right - works!
+        /*
+        gyroDrive(-0.3, -2500, 0);
+        gyroTurn(0.3, 90);
+        gyroHold(0.3, 90, 0.5);
+        gyroDrive(-0.3, LEFT_TICKS_RIGHT, 90);
+        gyroTurn(0.3, 0);
+        gyroHold(0.3, 0, 0.5);
+        gyroDrive(-0.3, -700, 90);
+        robot.cubePickUpSpeed(-1.0);
+        gyroDrive(-0.3, -800, 0);
+        robot.cubePickUpSpeed(0.0);
+        gyroDrive(-0.3, 480, 0);
+        robot.cubePickUpSpeed(0.0);
+        */
+
+        gyroDrive(-0.3, 3400, 0);
+        gyroTurn(0.3, 90);
+        gyroHold(0.3, 90, 0.5);
+        gyroDrive(-0.3, MIDDLE_TICKS_LEFT_B - 190, 90);
+        gyroTurn(0.3, 180);
+        gyroHold(0.3, 180, 0.5);
+        gyroDrive(-0.3, -800, 180);
+        robot.cubePickUpSpeed(-1.0);
+        gyroDrive(-0.3, -1000, 180);
+        robot.cubePickUpSpeed(0.0);
+        gyroDrive(-0.3, 480, 180);
+        robot.cubePickUpSpeed(0.0);
+
+
     }
 }
